ed5a7e2a48904edadb424aeffcf318d44ba67900
hncboy <619452863@qq.com>
2018-07-15 08:27:24 +0800

    完成后台的产品图片管理

---
+++ src/main/java/com/hncboy/tmall/service/impl/ProductImageServiceImpl.java
@@ -0,0 +1,47 @@
+package com.hncboy.tmall.service.impl;
+
+import com.hncboy.tmall.mapper.ProductImageMapper;
+import com.hncboy.tmall.pojo.ProductImage;
+import com.hncboy.tmall.pojo.ProductImageExample;
+import com.hncboy.tmall.service.ProductImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductImageServiceImpl implements ProductImageService {
+
+    @Autowired
+    private ProductImageMapper productImageMapper;
+
+    @Override
+    public void add(ProductImage pi) {
+        productImageMapper.insert(pi);
+    }
+
+    @Override
+    public void delete(int id) {
+        productImageMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(ProductImage pi) {
+        productImageMapper.updateByPrimaryKeySelective(pi);
+    }
+
+    @Override
+    public ProductImage get(int id) {
+        return productImageMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public List list(int pid, String type) {
+        ProductImageExample example = new ProductImageExample();
+        example.createCriteria()
+                .andPidEqualTo(pid)
+                .andTypeEqualTo(type);
+        example.setOrderByClause("id desc");
+        return productImageMapper.selectByExample(example);
+    }
+}
