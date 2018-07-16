168b6c27290645ce0478c9465d8f207da9487b30
hncboy <619452863@qq.com>
2018-07-16 10:14:25 +0800

    完成前台分类页

--- src/main/java/com/hncboy/tmall/controller/ForeController.java
+++ src/main/java/com/hncboy/tmall/controller/ForeController.java
@@ -2,6 +2,7 @@ package com.hncboy.tmall.controller;
 
 import com.hncboy.tmall.pojo.*;
 import com.hncboy.tmall.service.*;
+import comparator.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -128,5 +130,34 @@ public class ForeController {
         }
         session.setAttribute("user", user);
         return "success";
+    }
+
+    @RequestMapping("forecategory")
+    public String category(int cid, String sort, Model model) {
+        Category c = categoryService.get(cid);
+        productService.fill(c);
+        productService.setSaleAndReviewNumber(c.getProducts());
+
+        if (null != sort) {
+            switch (sort) {
+                case "review":
+                    Collections.sort(c.getProducts(), new ProductReviewComparator());
+                    break;
+                case "date":
+                    Collections.sort(c.getProducts(), new ProductDateComparator());
+                    break;
+                case "saleCount":
+                    Collections.sort(c.getProducts(), new ProductSaleCountComparator());
+                    break;
+                case "price":
+                    Collections.sort(c.getProducts(), new ProductPriceComparator());
+                    break;
+                case "all":
+                    Collections.sort(c.getProducts(), new ProductAllComparator());
+                    break;
+            }
+        }
+        model.addAttribute("c", c);
+        return "fore/category";
     }
 }
---
+++ src/main/java/comparator/ProductSaleCountComparator.java
@@ -0,0 +1,13 @@
+package comparator;
+
+import com.hncboy.tmall.pojo.Product;
+
+import java.util.Comparator;
+
+public class ProductSaleCountComparator implements Comparator<Product> {
+
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p2.getSaleCount() - p1.getSaleCount();
+    }
+}
